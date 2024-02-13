a4eeb80ec4d8f217caca20c244dbb5e3f88c7b11
Emanuel Borges <145613573+xMortall@users.noreply.github.com>
2024-02-13 19:26:44 +0000

    Update parte-3.cpp

--- parte-3.cpp
+++ parte-3.cpp
@@ -4,7 +4,7 @@ int main() {
     cout << "Digite um numero inteiro: ";
     cin >> n;
     cout << "Tabuada de " << n << ":" << endl;
-    for (int i = 1; i <= 10; ++i) {
+    for (int i = 1; i <= 10; i++) {
         cout << n << " x " << i << " = " << n * i << endl;
     }
     return 0;
